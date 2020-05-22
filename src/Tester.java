65b5e69e23fa6da896793476d3d5d2523eaaaefb
Maciej Mieczakowski <maciej.mieczakowski@gmail.com>
2020-05-22 14:48:14 +0200

    13.3 Ready for review Null values handling

--- src/Tester.java
+++ src/Tester.java
@@ -15,12 +15,12 @@ public class Tester {
         computers.add((new Computer("Dell Precision T3630", "3,2 GHz", 32)));
         computers.add((new Computer("Dell Precision T3000", "3,2 GHz", 32)));
         computers.add((new Computer("Dell Precision T3630", "3,0 GHz", 64)));
-        computers.add((new Computer("Apple", "3,0 GHz", 64)));
+        computers.add((new Computer("Apple", null, 64)));
 
         Comparator<Computer> sortOrder;
         try {
             sortOrder = sortListComparator(Utilities.runSortAction());
-            computers.sort(sortOrder);
+            Collections.sort(computers, sortOrder);
         } catch (IllegalStateException ex) {
             Collections.sort(computers);
             System.err.println("Wrong input value! Using default sort order by Name");
@@ -31,9 +31,9 @@ public class Tester {
 
     private static Comparator<Computer> sortListComparator(String sortOrder) {
         return switch (sortOrder) {
-            case "ram" -> new ComputerMemoryComparator().reversed();
-            case "cpu" -> new ComputerCpuComparator().reversed();
-            case "name" -> new ComputerNameComparator();
+            case "ram" -> Comparator.comparing(Computer::getMemory, Comparator.nullsFirst(Comparator.naturalOrder()));/*new ComputerMemoryComparator().reversed();*/
+            case "cpu" -> Comparator.comparing(Computer::getCpu, Comparator.nullsFirst(Comparator.naturalOrder()));
+            case "name" -> Comparator.comparing(Computer::getName, Comparator.nullsFirst(Comparator.naturalOrder()));
             default -> throw new IllegalStateException("Unexpected value: " + sortOrder);
         };
     }
